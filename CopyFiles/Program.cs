4f128634bfb9f626d6e3bf3b2fea96949f103c5c
Mehedi Hasan <mehedihasan93391@gmail.com>
2021-12-23 01:18:33 +0600

    Add project files.

---
+++ CopyFiles/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Source Path: ");
+var sourcePath = Console.ReadLine();
+Console.Write("Destination Path: ");
+var destinationPath = Console.ReadLine();
+Console.Write("Extension: ");
+var extension = Console.ReadLine();
+Console.WriteLine();
+Console.CursorVisible = false;
+
+Console.WriteLine("{0}", 0 + " files copied!");
+Console.WriteLine("{0}", 0 + " copy failed!");
+
+var tempPath = @"C:\TempPath\";
+var success = 0;
+var failed = 0;
+
+string[] allfiles = Directory.GetFiles(sourcePath + @"\", "*" + extension, SearchOption.AllDirectories);
+foreach (string file in allfiles)
+{
+	var fileName = Path.GetFileName(file);
+
+	if (File.Exists(file))
+	{
+		System.IO.File.Copy(sourcePath + @"\" + fileName, tempPath + fileName, true);
+		System.IO.File.Move(tempPath + fileName, destinationPath + @"\" + fileName, true);
+		success++;
+		Console.SetCursorPosition(0, 4);
+		Console.Write("{0}", success + " files copied!");
+	}
+	else
+	{
+		failed++;
+		Console.SetCursorPosition(0, 5);
+		Console.Write("{0}", failed + " copy failed!");
+		Console.WriteLine(fileName);
+	}
+}
+Console.Beep();
+Console.BackgroundColor = ConsoleColor.Green;
+
+Console.ReadLine();
